b8f1f62d9d0e4f4b9041231c263a1529eeaa560f
aser161 <aser161@yandex.ru>
2022-10-28 11:47:53 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задание 1
+        System.out.println("// Задание 1");
+        int age = 15;
+        if (age >= 18) {
+            System.out.println("Поздравляем с 18 летием");
+        }
+        if (age < 18) {
+            System.out.println("Вам нужно подождать до 18-ти лет");
+        }
+            // Задание 2
+            System.out.println("// Задание 2");
+            int age1 = 30;
+            if (age1 >= 7) {
+                System.out.println("Вам пора идти в школу");
+            }
+            if (age1 >= 18) {
+                System.out.println("Можете отправляться в университет");
+            }
+            if (age1 >= 24) {
+                System.out.println("Пора искать первую работу");
+            }
+                // Задание 3
+                System.out.println("// Задание 3");
+                int wagonCapacity = 105;
+                if (wagonCapacity <= 60) {
+                    System.out.println("Есть сидячие места");
+                }
+                if (wagonCapacity > 60) {
+                    System.out.println("Только стоячие места");
+                }
+                if (wagonCapacity >= 103) {
+                System.out.println("Мест нет");
+        }
+
+
+
+    }
+}
